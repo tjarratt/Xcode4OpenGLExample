75f8ff9839659b81f64999e4de32c37eb1fdf1ee
Tim Jarratt <tjarratt@gmail.com>
2013-07-07 14:26:16 -0700

    Initial Commit

---
+++ Xcode4OpenGLExample/GLGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GLGAppDelegate.h
+//  Xcode4OpenGLExample
+//
+//  Created by Tim Jarratt on 7/7/13.
+//  Copyright (c) 2013 General Linear Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GLGAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Xcode4OpenGLExample/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Xcode4OpenGLExample
+//
+//  Created by Tim Jarratt on 7/7/13.
+//  Copyright (c) 2013 General Linear Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
